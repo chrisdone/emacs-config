bf88746f2a78a1006069522f924cba754992a98c
Chris Done <chrisdone@gmail.com>
2014-01-30 12:53:15 +0100

    Remove me-specific details

--- config/email.el
+++ config/email.el
@@ -1,8 +1,1 @@
 (setq mail-user-agent 'message-user-agent)
-
-(setq user-mail-address "chrisdone@gmail.com"
-      user-full-name "Chris Done")
-
-(setq smtpmail-stream-type 'ssl
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 465)
