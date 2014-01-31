4e78e2aeff213a35def7448ba808242305209d5f
Chris Done <chrisdone@gmail.com>
2014-01-31 10:30:41 +0100

    Some keybindings for notmuch

--- config/email.el
+++ config/email.el
@@ -1,1 +1,4 @@
 (setq mail-user-agent 'message-user-agent)
+
+(define-key notmuch-search-mode-map (kbd "g") 'notmuch-refresh-this-buffer)
+(define-key notmuch-hello-mode-map (kbd "g") 'notmuch-refresh-this-buffer)
