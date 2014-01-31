c269a07d2f48089d7b326ff84c5d2736d94ae78d
Chris Done <chrisdone@gmail.com>
2014-01-31 11:10:51 +0100

    Some colors for sunburn

--- config/notmuch.el
+++ config/notmuch.el
@@ -7,3 +7,13 @@
       notmuch-search-line-faces
       `(("unread" :weight bold)
         ("flagged" :foreground ,zenburn-blue)))
+
+(setq notmuch-tag-formats
+      `(("unread" (propertize tag 'face '(:foreground ,sunburn-red)))
+        ("flagged" (notmuch-tag-format-image-data tag (notmuch-tag-star-icon))))
+      notmuch-show-logo nil
+      notmuch-hello-thousands-separator ","
+      notmuch-search-oldest-first nil
+      notmuch-search-line-faces
+      `(("unread" :weight bold)
+        ("flagged" :foreground ,sunburn-blue)))
