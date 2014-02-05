fdfad37db51bc14b0e0eb5737c613174751fe135
Chris Done <chrisdone@gmail.com>
2014-02-05 14:24:26 +0100

    Clever dabbrev-expand for emacs-lisp-mode

---
+++ config/lisp.el
@@ -0,0 +1,25 @@
+
+;; Fundamental functions
+
+(defun emacs-lisp-expand-clever (arg)
+  "Cleverly expand symbols with normal dabbrev-expand, but also
+  if the symbol is -foo, then expand to module-name-foo."
+  (interactive "*P")
+  (let ((sym (dabbrev--abbrev-at-point)))
+    (cond
+     ((string-match "^-" sym)
+      (save-excursion
+        (backward-sexp 1)
+        (insert (emacs-lisp-module-name))))
+     (t (dabbrev-expand arg)))))
+
+(defun emacs-lisp-module-name ()
+  "Search the buffer for `provide' declaration."
+  (save-excursion
+    (search-forward-regexp "^(provide '")
+    (symbol-name (symbol-at-point))))
+
+
+;; Keybindings
+
+(define-key emacs-lisp-mode-map (kbd "M-/") 'emacs-lisp-expand-clever)
