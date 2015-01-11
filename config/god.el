48d0ee425b1f00d80f2f48f525cf4d5581394398
Chris Done <chrisdone@gmail.com>
2015-01-11 02:22:28 +0100

    Update god-mode

--- config/god.el
+++ config/god.el
@@ -1,11 +1,14 @@
+(require 'god-mode-isearch)
+
 
 ;; Functions
 
 (defun god-update-cursor ()
   "Update my cursor."
-  (setq cursor-type (if (or god-local-mode buffer-read-only)
-                        'box
-                      'bar)))
+  (setq cursor-type
+        (if god-local-mode
+            'box
+          'bar)))
 
 (defun god-toggle-on-overwrite ()
   "Toggle god-mode on overwrite-mode."
@@ -20,11 +23,17 @@
 (global-set-key (kbd "C-x C-2") 'split-window-below)
 (global-set-key (kbd "C-x C-3") 'split-window-right)
 (global-set-key (kbd "C-x C-0") 'delete-window)
-(global-set-key (kbd "<escape>") 'god-local-mode)
 
 (define-key god-local-mode-map (kbd ".") 'repeat)
-(define-key god-local-mode-map (kbd "[") 'paredit-open-square)
-(define-key god-local-mode-map (kbd "]") 'paredit-close-square)
+(define-key god-local-mode-map (kbd "DEL") nil)
+
+(global-set-key (kbd "<escape>") 'god-mode-all)
+
+(define-key isearch-mode-map (kbd "<escape>") 'god-mode-isearch-activate)
+(define-key god-mode-isearch-map (kbd "<escape>") 'god-mode-isearch-disable)
+
+(setq god-exempt-major-modes nil)
+(setq god-exempt-predicates nil)
 
 
 ;; Hooks
