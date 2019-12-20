a6380692b1cb6f664bd8e32fdacf25fed4cbeae0
Chris Done <chrisdone@gmail.com>
2019-12-20 12:43:21 +0000

    Update markdown config

--- config/markdown.el
+++ config/markdown.el
@@ -1,21 +1,23 @@
 (require 'markdown-mode)
 
 (defvar markdown-code-languages
-  '("haskell" "lisp" "javascript" "c"))
+  '("haskell" "lisp" "javascript" "c" "yaml"))
 
 (defun markdown-code-fence (beg end)
   "Make a code fence of the given region."
   (interactive "r")
-  (save-excursion
-    (goto-char beg)
-    (goto-char (line-beginning-position))
-    (insert "``` "
-            (ido-completing-read "Language: " markdown-code-languages)
-            "\n")
-    (goto-char end)
-    (goto-char (line-end-position))
-    (newline)
-    (insert "```")))
+  (let ((lang (ido-completing-read "Language: " markdown-code-languages)))
+    (save-excursion
+      (goto-char end)
+      (goto-char (line-end-position))
+      (newline)
+      (insert "```")
+      (goto-char beg)
+      (goto-char (line-beginning-position))
+      (insert "``` "
+              lang
+              "\n")
+      (goto-char beg))))
 
 (define-key markdown-mode-map (kbd "C-c C-f") 'markdown-code-fence)
 (define-key markdown-mode-map (kbd "M-;") 'markdown-blockquote-region)
@@ -25,3 +27,5 @@
   (interactive)
   (save-buffer)
   (shell-command-to-string "git add .; git commit -m emacs-refresh; git push"))
+
+(define-key markdown-mode-map (kbd "C-v C-n") 'flyspell-goto-next-error)
