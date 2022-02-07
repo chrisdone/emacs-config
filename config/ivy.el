fce770a6e3f9b844559e3b090ba78e405741e39b
Chris Done <git@chrisdone.com>
2022-02-07 11:23:53 +0000

    Tweak ivy.el

--- config/ivy.el
+++ config/ivy.el
@@ -1,4 +1,18 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 
+(let ((completing-read-function 'ivy-completing-read))
+  (call-interactively 'project-search))
+
+(defun ls-files ()
+  (interactive)
+  (find-file
+   (ivy-completing-read
+    "Find File: "
+    (split-string (shell-command-to-string "git ls-files") "\n"))))
+
+(defun rg ()
+  (interactive)
+  (call-interactively 'counsel-rg))
+
 (provide 'ivy)
