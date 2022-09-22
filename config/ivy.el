432f627ef42fd91ed92e49bad3dd412b54192e15
Chris Done <git@chrisdone.com>
2022-09-22 09:17:37 +0100

    Drop accidentally left code

--- config/ivy.el
+++ config/ivy.el
@@ -1,9 +1,6 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 
-(let ((completing-read-function 'ivy-completing-read))
-  (call-interactively 'project-search))
-
 (defun ls-files ()
   (interactive)
   (let ((default-directory (car (split-string (shell-command-to-string "git rev-parse --show-toplevel")))))
