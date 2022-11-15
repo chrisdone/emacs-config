5511b97f977d21d9e5f316d2302a58e6bb80e996
Chris Done <chris.done@artificial.io>
2022-11-15 10:43:05 +0000

    Add h98-mode

---
+++ packages/h98-mode/h98-mode.el
@@ -0,0 +1,21 @@
+(defconst h98-keywords
+  `((" -- .*$" . font-lock-comment-face)
+    ("{-.*-}" . font-lock-comment-face)
+    (" --$" . font-lock-comment-face)
+    ("^--.*" . font-lock-comment-face)
+    ("\"\\(\\(\\\\\"\\)\\|[^\"]\\)*\"" . font-lock-string-face)
+    ("\\<\\*?\\[?[A-Z][a-zA-Z]*\\]?\\>" . font-lock-type-face)
+    (,(concat "\\<" (regexp-opt '("import" "module" "qualified" "as" "class" "instance" "where" "data" "type" "newtype" "do" "let" "$")) "\\>") . font-lock-keyword-face)
+    ("^[a-zA-Z_']+ " . font-lock-function-name-face)))
+
+
+(define-derived-mode h98-mode
+   prog-mode "H98"
+   "Major mode for Haskell 98.
+ \\{h98-mode-map}"
+   (set (make-local-variable 'font-lock-defaults) '(h98-keywords t nil nil))
+   (setq-local comment-start "-- ")
+   (setq-local comment-start-skip "-- ")
+   (setq-local comment-end ""))
+
+(provide 'h98-mode)
