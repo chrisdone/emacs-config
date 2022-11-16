cfba4a8462824dca7e7dfc9c216f03fabc70755c
Chris Done <chris.done@artificial.io>
2022-11-16 10:00:45 +0000

    Drop old stuff

--- old/config/email.el
+++
@@ -1,3 +0,0 @@
-(setq mail-user-agent 'message-user-agent)
-(custom-set-variables
- '(send-mail-function (quote smtpmail-send-it)))
--- old/packages/haskell-mode/haskell-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "haskell-mode" "16.1-git" "A Haskell editing mode"
-  '((emacs "24.1") (cl-lib "0.5"))
-  :url "https://github.com/haskell/haskell-mode"
-  :keywords '("haskell" "cabal" "ghc" "repl"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
