79936bbf9497620f618bce1c0d9b769d3fab4504
Chris Done <chris.done@artificial.io>
2022-11-17 13:52:47 +0000

    Better regex

--- packages/h98-mode/h98-mode.el
+++ packages/h98-mode/h98-mode.el
@@ -1,14 +1,3 @@
-(defconst h98-keywords
-  `((" -- .*$" . font-lock-comment-face)
-    ("{-.*-}" . font-lock-comment-face)
-    (" --$" . font-lock-comment-face)
-    ("^--.*" . font-lock-comment-face)
-    ("\"\\(\\(\\\\\"\\)\\|[^\"]\\)*\"" . font-lock-string-face)
-    ("\\<\\*?\\[?[A-Z][a-zA-Z]*\\]?\\>" . font-lock-type-face)
-    (,(concat "\\<" (regexp-opt '("deriving" "case" "of" "stock" "anyclass" "via" "import" "module" "qualified" "as" "class" "instance" "where" "data" "type" "newtype" "do" "let" "$")) "\\>") . font-lock-keyword-face)
-    ("^[a-zA-Z_']+ " . font-lock-function-name-face)))
-
-
 (define-derived-mode h98-mode
    prog-mode "H98"
    "Major mode for Haskell 98.
@@ -17,6 +6,16 @@
    (setq-local comment-start "-- ")
    (setq-local comment-start-skip "-- ")
    (setq-local comment-end ""))
+
+(defconst h98-keywords
+  `((" -- .*$" . font-lock-comment-face)
+    ("{-.*-}" . font-lock-comment-face)
+    (" --$" . font-lock-comment-face)
+    ("^--.*" . font-lock-comment-face)
+    ("\"\\(\\(\\\\\"\\)\\|[^\"]\\)*\"" . font-lock-string-face)
+    ("\\<\\*?\\[?[A-Z][a-zA-Z0-9_']*\\]?\\>" . font-lock-type-face)
+    (,(concat "\\<" (regexp-opt '("deriving" "case" "of" "stock" "anyclass" "via" "import" "module" "qualified" "as" "class" "instance" "where" "data" "type" "newtype" "do" "let" "$")) "\\>") . font-lock-keyword-face)
+    ("^[a-zA-Z_'0-9]+ " . font-lock-function-name-face)))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.hs\\'" . h98-mode))
