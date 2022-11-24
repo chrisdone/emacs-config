3f1b994442e35d6cb649c71c96465bc35c0132c5
Chris Done <chris.done@artificial.io>
2022-11-24 14:35:11 +0000

    Add hook for company on h98

--- config/hooks.el
+++ config/hooks.el
@@ -4,3 +4,4 @@
 (add-hook 'magit-status-mode-hook 'turn-off-linum-mode)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
+(add-hook 'h98-mode-hook 'company-mode)
