49b7536adb16a42980037f360cb5967e884fc677
Chris Done <git@chrisdone.com>
2023-05-04 09:40:32 +0100

    Change rg path to  ~/

--- config/ivy.el
+++ config/ivy.el
@@ -1,4 +1,4 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 (setq counsel-rg-base-command
-   '("/Users/chris/.nix-profile/bin/rg" "--max-columns" "240" "--with-filename" "--no-heading" "--line-number" "--color" "never" "%s"))
+   '("~/.nix-profile/bin/rg" "--max-columns" "240" "--with-filename" "--no-heading" "--line-number" "--color" "never" "%s"))
