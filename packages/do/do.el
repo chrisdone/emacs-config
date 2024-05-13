b2355176e1b0b598330b30784751101caaf97863
Chris Done <chris.done@artificial.io>
2024-05-13 14:58:47 +0100

    wip

--- packages/do/do.el
+++ packages/do/do.el
@@ -5,25 +5,27 @@
 ;; event-schedule =
 ;;   from <point-in-time> every <duration> (until <point-in-time>)?
 
-(defconst do-date-rx
-  (rx
-   (and word-start
-         (or
-          ;; today/tomorrow
-          (and (group-n 1 "tom") (optional "orrow"))
-          (or (and (group-n 1 "tod") (optional "ay")))
-          ;; next <thing>
-          (and "next "
-               (group-n 2 (or "week" "month" "year")))
-          ;; in x <things>
-          (and "in "
-               (group-n 3 (or "a" (one-or-more digit)))
-               " "
-               (group-n 4 (or "day" "week" "month" "year"))
-               (optional "s")))
-         word-end)))
+(rx-define do-duration-rx
+  (and (group-n 3 (or "a" (one-or-more digit)))
+       " "
+       (group-n 4 (or "day" "week" "month" "year"))
+       (optional "s")))
 
-(defun do-parse-date-days (str)
+(rx-define do-date-rx
+  (and word-start
+       (or
+        ;; today/tomorrow
+        (and (group-n 1 "tom") (optional "orrow"))
+        (or (and (group-n 1 "tod") (optional "ay")))
+        ;; next <thing>
+        (and "next "
+             (group-n 2 (or "week" "month" "year")))
+        ;; in x <things>
+        (and "in "
+             do-duration-rx))
+       word-end))
+
+(defun do-parse-date-rx (str)
   "Parse the result of `do-date-rx' from LIST and STR into an integer of days."
   (cond
    ;; today/tomorrow
@@ -34,7 +36,12 @@
    ((string= (match-string 2 str) "month") `(:next-month))
    ((string= (match-string 2 str) "year") `(:next-year))
    ;; in x <things>.
-   ((string= (match-string 3 str) "a") `(:in-n-things . (:count 1 :thing (match-string 4 str))))
+   ((string= (match-string 3 str) "a")
+    `(:n-things
+      . (:count 1 :thing ,(match-string 4 str))))
+   ((match-string 3 str)
+    `(:n-things
+      . (:count ,(string-to-number (match-string 3 str)) :thing ,(match-string 4 str))))
    ))
 
 (defconst do-priority-rx
