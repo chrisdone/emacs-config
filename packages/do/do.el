c0c8971a973ae57e050eeede944de7d22adea061
Chris Done <chris.done@artificial.io>
2024-05-13 14:01:44 +0100

    wip

--- packages/do/do.el
+++ packages/do/do.el
@@ -1,3 +1,10 @@
+;; point-in-time = next <day/week/month/year>, in <duration>
+;;
+;; duration = n-days/week/month/year (work days)
+;;
+;; event-schedule =
+;;   from <point-in-time> every <duration> (until <point-in-time>)?
+
 (defconst do-date-rx
   (rx
    (and word-start
@@ -7,7 +14,7 @@
           (or (and (group-n 1 "tod") (optional "ay")))
           ;; next <thing>
           (and "next "
-               (group-n 2 (or "day" "week" "month" "year")))
+               (group-n 2 (or "week" "month" "year")))
           ;; in x <things>
           (and "in "
                (group-n 3 (or "a" (one-or-more digit)))
@@ -15,6 +22,20 @@
                (group-n 4 (or "day" "week" "month" "year"))
                (optional "s")))
          word-end)))
+
+(defun do-parse-date-days (str)
+  "Parse the result of `do-date-rx' from LIST and STR into an integer of days."
+  (cond
+   ;; today/tomorrow
+   ((string= (match-string 1 str) "tod") `(:today))
+   ((string= (match-string 1 str) "tom") `(:tomorrow))
+   ;; next <thing>
+   ((string= (match-string 2 str) "week") `(:next-week))
+   ((string= (match-string 2 str) "month") `(:next-month))
+   ((string= (match-string 2 str) "year") `(:next-year))
+   ;; in x <things>.
+   ((string= (match-string 3 str) "a") `(:in-n-things . (:count 1 :thing (match-string 4 str))))
+   ))
 
 (defconst do-priority-rx
   (rx
