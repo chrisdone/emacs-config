6adadf5cf0259b8294fce41651fee87b4250a135
Chris Done <chris.done@artificial.io>
2025-07-03 22:08:45 +0100

    Drop commentary

--- packages/llm/llm.el
+++ packages/llm/llm.el
@@ -1,5 +1,3 @@
-;; (llm-query-sync-response "one line answer: ollama default port")
-
 (defvar llm-host-port 'llm-default-host-port)
 
 (defun llm-default-host-port ()
@@ -8,7 +6,7 @@
 (make-process
  :name "llm"
  :buffer (generate-new-buffer "*llm-stream*")
- :command (list "curl" (concat "http://" (funcall llm-host-port) "/api/generate") "--no-buffer"  "-d" "{\"model\": \"deepseek-r1:7b\", \"prompt\": \"random quote\", \"options\": {\"num_predict\":10}  }, \"stream\": true}" "--silent")
+ :command (list "curl" (concat "http://" (funcall llm-host-port) "/api/generate") "--no-buffer"  "-d" "{\"model\": \"deepseek-r1:7b\", \"prompt\": \"random quote\", \"options\": {\"num_predict\":20}  }, \"stream\": true}" "--silent")
  :connection-type 'pipe
  :filter 'llm-process-filter)
 
@@ -18,46 +16,4 @@
     (cl-loop for message = (condition-case nil (json-parse-buffer :object-type 'plist)
                              (json-end-of-file nil))
              while message
-             do (message "message: %S" message))))
-
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.639906Z" :response " community" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.683592Z" :response " growing" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.742197Z" :response " in" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.800439Z" :response " machine" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.844474Z" :response " libraries" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.859124Z" :response " such" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.902762Z" :response " and" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:04.96144Z" :response "5" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.005091Z" :response "Performance" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.063575Z" :response " enough" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.078278Z" :response " for" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.093227Z" :response " most" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.137546Z" :response " though" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.196089Z" :response " slower" :done :false)
-;; message: (:model "deepseek-r1:7b" :created_at "2025-07-03T15:51:05.239779Z" :response " languages" :done :false)
-
-
-;; It's a sequence of messages that could be piped to an Emacs buffer,
-;; and then a JSON parser could walk across it, consuming messages,
-;; but retaining position. Each message would trigger a callback event
-;; handler, until the final one indicating closed. A synchronous call
-;; built on the async one would be a good test.
-;;
-;; Multiple simultaneous requests seem to work too.
-
-;; IMPORTANT: --no-buffer
-
-;; Streaming protocol example:
-;;
-;; chris@Chriss-MacBook-Pro ~ % curl http://localhost:8080/api/generate --no-buffer  -d '{"model": "deepseek-r1:7b", "prompt": "is haskell the best programming language?", "options": {"num_predict":10}  }, "stream": true}' --silent
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.607244Z","response":"\u003cthink\u003e","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.622323Z","response":"\n","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.635501Z","response":"Okay","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.648725Z","response":",","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.662088Z","response":" so","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.675341Z","response":" I","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.688821Z","response":"'m","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.702438Z","response":" trying","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.716038Z","response":" to","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.729821Z","response":" figure","done":false}
-;; {"model":"deepseek-r1:7b","created_at":"2025-07-02T10:39:37.729825Z","response":"","done":true,"done_reason":"length","context":[151644,285,702,74,613,279,1850,15473,4128,30,151645,151648,198,32313,11,773,358,2776,4460,311,7071],"total_duration":320742791,"load_duration":29775416,"prompt_eval_count":12,"prompt_eval_duration":167150291,"eval_count":10,"eval_duration":123079584}
+             do (message "%s" (plist-get message :response)))))
